5f7d4edcb01997d6e7b55a4da0193deabd2e5abc
dudeja-abhinav <dudeja.abhinav55@gmail.com>
2025-06-07 23:21:19 +0530

    Minor changes

--- Wordle/src/main/java/game/wordle/GamePanel.java
+++ Wordle/src/main/java/game/wordle/GamePanel.java
@@ -2,13 +2,36 @@ package game.wordle;
 
 import javax.swing.*;
 
+import java.awt.*;
+
+import static game.wordle.util.Constants.SCREEN_SIZE;
+
 public class GamePanel extends JPanel implements Runnable {
 
-    public GamePanel(){
+    private final Thread gameThread;
+    private Image image;
+    private Graphics graphics;
 
+    public GamePanel(){
+        this.setFocusable(true);
+        this.setPreferredSize(SCREEN_SIZE);
+        gameThread = new Thread(this);
+        gameThread.start();
     }
     @Override
     public void run() {
 
     }
+
+    public void paint(Graphics g){
+        image = createImage(getWidth(), getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public void draw(Graphics g){
+
+    }
+
 }
